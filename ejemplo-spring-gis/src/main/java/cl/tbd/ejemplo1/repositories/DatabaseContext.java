ceae21ecddf2294bc93c43ecceba2dd6f861c437
Gabriela Fernández R <gabriela.fernandez.r@usach.cl>
2022-05-15 17:19:03 -0400

    [Gabriela Fernández] - Conexión a BD hecha pero no probada

--- ejemplo-spring-gis/src/main/java/cl/tbd/ejemplo1/repositories/DatabaseContext.java
+++ ejemplo-spring-gis/src/main/java/cl/tbd/ejemplo1/repositories/DatabaseContext.java
@@ -7,17 +7,17 @@ import org.sql2o.Sql2o;
 
 @Configuration
 public class DatabaseContext {
-    @Value("${database.url}")
+    /*@Value("${database.url}")
     private String dbUrl;
 
     @Value("${database.user}")
     private String dbUser;
 
     @Value("${database.password}")
-    private String dbPass;
+    private String dbPass;*/
     
     @Bean
     public Sql2o sql2o(){
-        return new Sql2o(dbUrl, dbUser, dbPass);
+        return new Sql2o("jdbc:postgresql://127.0.0.1:5432/TareaTBD", "postgres", "postgresql");
     }
 }
