92f0223f3618f04fbb3109fe729d9683afb91135
Gabriela Fernández R <gabriela.fernandez.r@usach.cl>
2022-05-15 20:11:59 -0400

    [Gabriela Fernández] - SCRIPT Tabla Perros

--- scripts/perros.sql
+++ scripts/perros.sql
@@ -2,3 +2,75 @@ CREATE TABLE perror (id serial, name varchar, geom geometry(Point, 4326));
 
 INSERT INTO perror (name, geom)
 VALUES ('A', ST_GeomFromText('POINT(-72.928177 -41.474096)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('B', ST_GeomFromText('POINT(-70.690818 -33.450437)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('C', ST_GeomFromText('POINT(-70.508177 -33.344096)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('D', ST_GeomFromText('POINT(-70.687112 -33.450148)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('E', ST_GeomFromText('POINT(-70.688973 -33.450863)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('F', ST_GeomFromText('POINT(-70.682896 -33.450485)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('G', ST_GeomFromText('POINT(-70.682784 -33.448042)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('H', ST_GeomFromText('POINT(-70.697295 -33.532040)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('I', ST_GeomFromText('POINT(-70.764504 -33.515854)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('J', ST_GeomFromText('POINT(-70.659565 -33.463646)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('K', ST_GeomFromText('POINT(-70.644529 -33.474098)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('L', ST_GeomFromText('POINT(-70.711438 -33.510003)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('M', ST_GeomFromText('POINT(-70.644716 -33.423945)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('N', ST_GeomFromText('POINT(-71.538096 -33.049377)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('O', ST_GeomFromText('POINT(-73.1515 -39.50000)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('P', ST_GeomFromText('POINT(-71.536846 -33.024030)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('Q', ST_GeomFromText('POINT(-72.951792 -41.474745)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('R', ST_GeomFromText('POINT(-72.597633 -38.747735)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('S', ST_GeomFromText('POINT(-71.3038 -33.2114)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('T', ST_GeomFromText('POINT(-71.3214 -33.0125)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('U', ST_GeomFromText('POINT(-71.0507 -32.5707)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('V', ST_GeomFromText('POINT(-71.308177 -31.304096)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('X', ST_GeomFromText('POINT(-69.928177 -23.474096)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('Y', ST_GeomFromText('POINT(-69.228177 -23.374096)', 4326));
+
+INSERT INTO perror (name, geom)
+VALUES ('Z', ST_GeomFromText('POINT(-70.728177 -30.674096)', 4326));
